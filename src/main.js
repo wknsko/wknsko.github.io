c31eff2d194a46936ae4ac1986960034fa164aae
Na Ko <wknsko@gmail.com>
2022-09-28 15:29:42 +0200

    disable aos on mobile

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-AOS.init({disable: "phone"})
+AOS.init({disable: 'mobile'});
 createApp(App).use(AOS).mount('#app');
 
